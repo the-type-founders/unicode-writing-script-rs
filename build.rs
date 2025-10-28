f1effaffae2a29477e8865ded135e4c8cbb71aa2
Ivan Ukhov <ivan.ukhov@gmail.com>
2025-10-28 11:35:07 +0100

    Set up data.rs

--- build.rs
+++ build.rs
@@ -1,7 +1,25 @@
-use std::collections::HashMap;
+use std::collections::BTreeMap;
+use std::fs::File;
+use std::io::Write;
+use std::path::Path;
 
 const DATA: &str = include_str!("vendor/detect-writing-script/data.json");
 
+macro_rules! ok(
+    ($result:expr) => ($result.unwrap());
+);
+
 fn main() {
-    let _: HashMap<String, Vec<[usize; 2]>> = serde_json::from_str(DATA).unwrap();
+    let data: BTreeMap<String, Vec<[u32; 2]>> = ok!(serde_json::from_str(DATA));
+    let script_count = data.len();
+
+    let root = ok!(std::env::var("OUT_DIR"));
+    let path = Path::new(&root).join("data.rs");
+    let mut file = ok!(File::create(&path));
+    ok!(write!(
+        file,
+        r#"
+const SCRIPT_COUNT: usize = {script_count};
+        "#,
+    ));
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,21 @@
 //! Detection of writing scripts from Unicode codepoints.
+
+include!(concat!(env!("OUT_DIR"), "/data.rs"));
+
+/// A match.
+#[derive(Clone, Copy)]
+pub struct Match {
+    pub name: &'static str,
+    pub count: usize,
+    pub score: f64,
+}
+
+/// Detect writing scripts given ranges of Unicode codepoints.
+pub fn detect<T>(_codepoints: T, _threshold: f64) -> Vec<Match>
+where
+    T: IntoIterator<Item = [u32; 2]>,
+{
+    let _ = [0; SCRIPT_COUNT];
+
+    unimplemented!()
+}
